72fcb311e464b55a4e00bf74dbc1a6709b6f4365
Charles Loureiro de Deus <charliedeus@gmail.com>
2022-10-13 09:32:35 -0300

    first commit

---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Checkbox, CheckboxProps } from './Checkbox'
+import { Text } from './Text'
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  args: {},
+  argTypes: {},
+  decorators: [
+    (Story) => {
+      return (
+        <label className="flex items-center gap-2 hover:cursor-pointer">
+          {Story()} <Text size="sm">Lembrar-me de mim por 30 dias</Text>
+        </label>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Default: StoryObj<CheckboxProps> = {}
